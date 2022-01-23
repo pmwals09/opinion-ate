5fa5c20ff78c2114d24f5676735f4153f4a1ee1b
Pat Walsh <pmwals09@gmail.com>
2022-01-23 18:06:42 -0500

    Basic creating a restaurant functionality

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -2,6 +2,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import RestaurantList from './RestaurantList';
+import NewRestaurantForm from './NewRestaurantForm';
 
 const RestaurantScreen = () => {
   return (
@@ -9,6 +10,7 @@ const RestaurantScreen = () => {
       <CardContent>
         <Typography variant="h5">Restaurants</Typography>
         <RestaurantList />
+        <NewRestaurantForm />
       </CardContent>
     </Card>
   );
